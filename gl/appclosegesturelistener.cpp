4ecd6261c260c15357683c78256634bc0f3a98aa
BobNobrain <nikita-ivanov.mail@yandex.ru>
2017-06-09 20:41:10 +0300

    Added AppCloseGL

--- gl/appclosegesturelistener.cpp
+++ gl/appclosegesturelistener.cpp
@@ -1,6 +1,54 @@
 #include "appclosegesturelistener.h"
 
-AppCloseGestureListener::AppCloseGestureListener()
+AppCloseGestureListener::AppCloseGestureListener(int gestId, bool twice)
 {
-
+	gest = gestId;
+	repeat = twice;
+	done_once = false;
 }
+AppCloseGestureListener::~AppCloseGestureListener() {}
+
+void AppCloseGestureListener::onGestureDetected(int gestureId)
+{
+	if (gestureId == gest)
+	{
+		if (done_once || !repeat)
+		{
+			std::cout << "AppCloseGestureListener: closing application!" << std::endl;
+			exit(0);
+		}
+		else
+		{
+			done_once = true;
+			std::cout << "Repeat this gesture to close the application" << std::endl;
+		}
+	}
+	else
+	{
+		done_once = false;
+	}
+}
+
+class AppCloseGLFactory: public ModuleFactory
+{
+public:
+	AppCloseGLFactory()
+	{
+		BasicModule::registerModule("appclose", this);
+	}
+
+	BasicModule* create(const nlohmann::json &params)
+	{
+		int gid = params["gesture_id"];
+		bool twice = true;
+		auto search = params.find("repeat_twice");
+		if (search != params.end())
+		{
+			twice = search.value();
+		}
+		AppCloseGestureListener* result = new AppCloseGestureListener(gid, twice);
+		return result;
+	}
+};
+
+static AppCloseGLFactory global_AppCloseGLFactory;
--- gl/appclosegesturelistener.h
+++ gl/appclosegesturelistener.h
@@ -1,11 +1,22 @@
 #ifndef APPCLOSEGESTURELISTENER_H
 #define APPCLOSEGESTURELISTENER_H
 
+#include "common/abstractgesturelistener.h"
 
-class AppCloseGestureListener
+class AppCloseGestureListener: public AbstractGestureListener
 {
 public:
-	AppCloseGestureListener();
+	AppCloseGestureListener(int gestId, bool twice);
+	~AppCloseGestureListener();
+
+	virtual void onGestureDetected(int gestureId) override;
+
+protected:
+	bool repeat;
+	int gest;
+
+private:
+	bool done_once;
 };
 
 #endif // APPCLOSEGESTURELISTENER_H
